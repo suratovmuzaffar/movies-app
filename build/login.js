aa94a9a8108f35640a14f3e5f2217102109518df
suratovmuzaffar <suratovmuzaffar77@gmail.com>
2025-03-19 16:56:15 +0300

    pagedan pagega malumot otkazildi

--- build/login.js
+++ build/login.js
@@ -1,2 +1,9 @@
 // src/login.ts
 var params = new URLSearchParams(window.location.search);
+var people = [];
+people.push({
+  username: params.get("reg-username"),
+  email: params.get("reg-email"),
+  password: params.get("reg-password")
+});
+console.log(people);
--- src/login.ts
+++ src/login.ts
@@ -1,2 +1,8 @@
 const params = new URLSearchParams(window.location.search);
-let object = {};
+let people = [];
+people.push({
+  username: params.get("reg-username"),
+  email: params.get("reg-email"),
+  password: params.get("reg-password"),
+});
+console.log(people);
