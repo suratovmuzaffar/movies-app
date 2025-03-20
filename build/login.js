0e1b6b405202adcc8daa82b831a41bfc243f2a29
suratovmuzaffar <suratovmuzaffar77@gmail.com>
2025-03-20 15:46:15 +0300

    register login menu toliq tugatildi

--- build/login.js
+++ build/login.js
@@ -1,9 +1,20 @@
 // src/login.ts
 var params = new URLSearchParams(window.location.search);
-var people = [];
-people.push({
+var regpeople = JSON.parse(localStorage.getItem("regpeople") || "[]");
+regpeople.push({
   username: params.get("reg-username"),
   email: params.get("reg-email"),
   password: params.get("reg-password")
 });
-console.log(people);
+console.log(regpeople);
+localStorage.setItem("regpeople", JSON.stringify(regpeople));
+var logpeople = {
+  email: params.get("log-email"),
+  password: params.get("log-password")
+};
+console.log(logpeople);
+for (let i = 0;i < regpeople.length; i++) {
+  if (regpeople[i].email === logpeople.email && regpeople[i].password === logpeople.password) {
+    document.querySelector("#login-form")?.setAttribute("action", "./index.html");
+  }
+}
--- src/login.ts
+++ src/login.ts
@@ -1,8 +1,28 @@
 const params = new URLSearchParams(window.location.search);
-let people = [];
-people.push({
+
+const regpeople = JSON.parse(localStorage.getItem("regpeople") || "[]");
+regpeople.push({
   username: params.get("reg-username"),
   email: params.get("reg-email"),
   password: params.get("reg-password"),
 });
-console.log(people);
+console.log(regpeople);
+
+localStorage.setItem("regpeople", JSON.stringify(regpeople));
+
+let logpeople = {
+  email: params.get("log-email"),
+  password: params.get("log-password"),
+};
+console.log(logpeople);
+
+for (let i = 0; i < regpeople.length; i++) {
+  if (
+    regpeople[i].email === logpeople.email &&
+    regpeople[i].password === logpeople.password
+  ) {
+    document
+      .querySelector("#login-form")
+      ?.setAttribute("action", "./index.html");
+  }
+}
