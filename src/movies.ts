2d6186e222ec7223dadd6fe55416767559d73f48
suratovmuzaffar <suratovmuzaffar77@gmail.com>
2025-03-21 14:56:44 +0300

    list add  block qoshildi

--- src/movies.ts
+++ src/movies.ts
@@ -1,1 +1,22 @@
 const params = new URLSearchParams(window.location.search);
+interface Movies {
+  titlr: string;
+  ganre: string;
+  stock: number;
+  rate: number;
+  like: boolean;
+}
+let movies = [
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+  { titlr: "asasin", ganre: "action", stock: 7, rate: 3.5 },
+];
